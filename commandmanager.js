fdd116c917087008ac2424f31332a51f0b2b221e
thebarkerkids@hotmail.com <Ilov3popmusic>
2020-01-15 20:36:30 +0000

    another attempt at fixing problems

--- commandmanager.js
+++ commandmanager.js
@@ -61,6 +61,7 @@ commandmanager.onCommand = async function (msg, client) {
                     await msg.reply("You have been removed from the role 'Jackbox'.");
                     for (let x in msg.member.roles.keyArray()) {
                         if (ranks.includes(x.toString())) {
+                            console.log("testing");
                             return;
                         }
                     }
