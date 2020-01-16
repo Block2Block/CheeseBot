e9500050c7bf5533ba59212943aafab794916363
thebarkerkids@hotmail.com <Ilov3popmusic>
2020-01-16 14:22:21 +0000

    oops

--- mysqlmanager.js
+++ mysqlmanager.js
@@ -3,7 +3,7 @@ const MySQLClient = mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD,
-    database: process.env.BOT_DATABASE
+    database: process.env.MYSQL_DATABASE
 });
 
 function handleDisconnect() {// Recreate the connection, since
