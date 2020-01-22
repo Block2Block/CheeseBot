de807ae3ceb9d05945da29c12fc4630dd08630e1
thebarkerkids@hotmail.com <Ilov3popmusic>
2020-01-22 12:23:45 +0000

    Added viewable stack for logs

--- managers/CommandManager.js
+++ managers/CommandManager.js
@@ -115,7 +115,7 @@ commandManager.onCommand = async function (msg, client) {
                 dmchannel.send(help).then(() => {
                     msg.react("✅");
                 }).catch((err) => {
-                    console.log("Promise Rejection: " + err);
+                    console.log("Promise Rejection: " + err.stack);
                     msg.reply("You must enable PM's in order to use this command.")
                 });
 
