48653bd405d645a255c4427c2e9829dc28aed53d
Block2Block <thebarkerkids@hotmail.com>
2020-02-10 22:44:46 +0000

    Added mineman role and added Jake to livestream announcements

--- utils/Constants.js
+++ utils/Constants.js
@@ -12,7 +12,7 @@ const botConstants = {
     gameRole: "664626926127677440",
     nowPlayingChannel: "643571367715012638",
     commandPrefix: "!",
-    twitchSubscriptions: ["53100459","41451481","62033506"]
+    twitchSubscriptions: ["53100459","41451481","62033506","138310205"]
 };
 
 //Putting these here as this class does not use any initialising code (so the bot client doesnt get loaded like 5 times. (And to prevent circular dependencies).
