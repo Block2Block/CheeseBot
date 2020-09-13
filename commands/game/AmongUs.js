64543cfce701362bc3bbc0dabcb0c540c33cf64b
Block2Block <accounts@block2block.me>
2020-09-13 17:39:22 +0100

    Removing and adding some roles
    Signed-off-by: Block2Block <accounts@block2block.me>

---
+++ commands/game/AmongUs.js
@@ -0,0 +1,12 @@
+module.exports = {
+    cmd: "amongus",
+    arguments: "amongus",
+    aliases: [],
+    desc: "Join the Among Us Games game role.",
+    category: "game",
+    permission: "all",
+    allowed_channels: ["439114503171604480","439114294307717131"],
+    joinable_role: "754740985136808007",
+    allow_in_dm: false,
+    run: null
+};
--- commands/game/FallGuys.js
+++ commands/game/FallGuys.js
@@ -1,12 +1,12 @@
 module.exports = {
-    cmd: "clue",
-    arguments: "clue",
-    aliases: ["cluedo"],
-    desc: "Join the Clue/Cluedo game role.",
+    cmd: "fallguys",
+    arguments: "fallguys",
+    aliases: ["fallguy"],
+    desc: "Join the Fall Guys Games game role.",
     category: "game",
     permission: "all",
     allowed_channels: ["439114503171604480","439114294307717131"],
-    joinable_role: "664135506115690496",
+    joinable_role: "740933965774323784",
     allow_in_dm: false,
     run: null
 };
